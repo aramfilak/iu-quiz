cd94b0c5c603b6a8879378c61f8a6d14a42c4a5b
mairs <s.mair@palfinger.com>
2023-11-14 15:06:07 +0100

    Implement update API for Nickname

--- server/src/apis/student/services.ts
+++ server/src/apis/student/services.ts
@@ -28,6 +28,30 @@ async function findOne(req: Request, res: Response) {
     .json(createApiResponse(StatusCodes.OK, '', studentDataWithOutPassword));
 }
 
-async function update(req: Request, res: Response) {}
+/**
+ * Update Student data.
+ * @access protected
+ */
+async function update(req: Request, res: Response) {
+  const studentId = req.auth?.id;
+  const { newNickName } = req.body;
+
+  if (!newNickName) {
+    return res.status(StatusCodes.BAD_REQUEST).json(
+      createApiResponse(StatusCodes.BAD_REQUEST, 'Die Angabe eines Nicknames ist erforderlich!')
+    );
+  }
+
+  const updatedStudent = await database.student.update({
+    where: { id: studentId },
+    data: { nickName: newNickName }
+  });
+
+  const updatedStudentWithoutPassword = excludeObjectProperty('password', updatedStudent);
+
+  res.status(StatusCodes.OK).json(
+    createApiResponse(StatusCodes.OK, 'Die Aktualisierung ihres Nicknames wurde erfolgreich durchgeführt!', updatedStudentWithoutPassword)
+  );
+}
 
 export { findOne, update };
