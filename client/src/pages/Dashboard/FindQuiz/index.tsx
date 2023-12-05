35efdddc2273a6a23168683c9faac3933b039515
mairs <s.mair@palfinger.com>
2023-12-05 14:11:02 +0100

    #96 implement filter function for find quiz

--- client/src/pages/Dashboard/FindQuiz/index.tsx
+++ client/src/pages/Dashboard/FindQuiz/index.tsx
@@ -6,28 +6,15 @@ import {
   InputRightElement,
   Icon,
   useColorModeValue,
-  Checkbox,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
   Button,
   Flex,
   Tooltip,
   Heading,
-  Text,
-  IconButton,
-  useToast
+  useToast,
+  InputLeftAddon,
+  Select
 } from '@chakra-ui/react';
-import {
-  FaSearch,
-  FaChevronDown,
-  FaGraduationCap,
-  FaBook,
-  FaSync,
-  FaArrowUp,
-  FaArrowDown
-} from 'react-icons/fa';
+import { FaSearch, FaGraduationCap, FaBook, FaSync } from 'react-icons/fa';
 import { IoMdClose } from 'react-icons/io';
 import {
   PageHeader,
@@ -41,20 +28,18 @@ import courseOfStudy from '../../../data/courseOfStudy.json';
 import { QuizCard } from '../../../components/QuizCard';
 import { useFetch } from '../../../hooks';
 import { QuizQueryParams } from '../../../utils/types';
+import CustomCheckboxGroup from '../../../components/CustomCheckboxGroup';
 
 function FindQuiz() {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const { getAllQuizzes, followQuiz } = useQuizStore();
-  const [selectedCourse, setSelectedCourse] = useState<string | null>(null);
-  const [selectedModules, setSelectedModules] = useState<string[]>([]);
-  const [selectedSortOrder, setSelectedSortOrder] = useState<string | string | null>(
-    null
-  );
-  const [selectedFilterProperty, setSelectedFilterProperty] = useState<string | null>(
-    null
-  );
-  const [isBoxOpen, setIsBoxOpen] = useState(true);
-  const boxShadowDark = isBoxOpen ? '' : '1px solid #4a5568';
+  const [selectedCourseOfStudy, setSelectedCourseOfStudy] = useState<string>('');
+  const [selectedCourse, setSelectedCourse] = useState<string>('');
+  const [selectedSortOrder, setSelectedSortOrder] = useState<string | undefined>('asc');
+  const [selectedFilterProperty, setSelectedFilterProperty] = useState<
+    string | undefined
+  >(undefined);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const toast = useToast();
   const [params, setParams] = useState<QuizQueryParams>({ unFollowed: true });
   const {
@@ -77,39 +62,19 @@ function FindQuiz() {
     });
   };
 
-  const handleToggleBox = () => {
-    setIsBoxOpen((prev) => !prev);
-  };
-
   const handleFilterReset = () => {
-    setSelectedFilterProperty(null);
-    setSelectedSortOrder(null);
-    setSelectedCourse(null);
-    setSelectedModules([]);
+    handlePropertyChange(undefined);
+    setSelectedSortOrder('asc');
+    setSelectedCourseOfStudy('');
+    setSelectedCourse('');
   };
 
-  const handlePropertyChange = (property: string) => {
-    setSelectedFilterProperty((prev) => (prev === property ? null : property));
-    if (selectedFilterProperty === property) setSelectedSortOrder(null);
+  const handlePropertyChange = (property: string | undefined) => {
+    setSelectedFilterProperty(property);
   };
 
-  const handleSortChange = (sort: string) => {
-    setSelectedSortOrder((prev) => (prev === sort ? null : sort));
-  };
-
-  const handleCourseChange = (course: string) => {
-    setSelectedCourse((prev) => (prev === course ? null : course));
-    if (!course) {
-      setSelectedModules([]);
-    }
-  };
-
-  const handleModuleChange = (module: string) => {
-    const updatedCourses = selectedModules.includes(module)
-      ? selectedModules.filter((c) => c !== module)
-      : [...selectedModules, module];
-
-    setSelectedModules(updatedCourses);
+  const handleSortChange = (sort: string | undefined) => {
+    setSelectedSortOrder(sort);
   };
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -120,225 +85,200 @@ function FindQuiz() {
     setSearchTerm('');
   };
 
+  const handleCourseOfStudyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCourseOfStudy = e.target.value;
+    setSelectedCourseOfStudy(selectedCourseOfStudy);
+  };
+
+  const handleCourseChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCourse = e.target.value;
+    setSelectedCourse(selectedCourse);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const selCourseOfStudy = selectedCourseOfStudy;
+    const selCourse = selectedCourse;
+    const selSortOrder = selectedSortOrder;
+    const selFilterProperty = selectedFilterProperty;
+
+    setParams({
+      unFollowed: true,
+      courseOfStudy: selCourseOfStudy,
+      course: selCourse,
+      size: selFilterProperty === 'size',
+      popularity: selFilterProperty === 'popularity',
+      updatedAt: selFilterProperty === 'updateAt',
+      sort: selSortOrder
+    });
+
+    refetchData();
+
+    setIsSubmitting(false);
+  };
+
   return (
     <>
       <Box mx="auto" maxW="600px" mb="4">
         <PageHeader title="Quiz Finden" description="Finde passende Quiz für dich." />
-        <Flex flexDir={{ base: 'column', sm: 'row' }} flexWrap="wrap" gap="1rem">
-          <InputGroup>
-            <Input
-              variant="outline"
-              type="text"
-              placeholder="Nach Quiz-Titel suchen ..."
-              value={searchTerm}
-              onChange={handleSearchChange}
-              bg={useColorModeValue('white', 'gray.800')}
-              borderColor={useColorModeValue('gray.300', 'gray.600')}
-              focusBorderColor="teal.500"
-            />
-            {searchTerm && (
-              <InputRightElement
-                children={
-                  <Icon
-                    as={IoMdClose}
-                    cursor="pointer"
-                    color="gray.300"
-                    fontSize="20px"
-                    onClick={handleDeleteSearch}
+        <form onSubmit={handleSubmit}>
+          <Flex flexDir={{ base: 'column', sm: 'row' }} flexWrap="wrap" gap="1rem">
+            <Flex w="100%" flexDir={'row'} gap="1rem">
+              <InputGroup>
+                <Input
+                  variant="outline"
+                  type="text"
+                  placeholder="Nach Quiz-Titel suchen ..."
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                  bg={useColorModeValue('white', 'gray.800')}
+                  borderColor={useColorModeValue('gray.300', 'gray.600')}
+                  focusBorderColor="teal.500"
+                />
+                {searchTerm && (
+                  <InputRightElement
+                    children={
+                      <Icon
+                        as={IoMdClose}
+                        cursor="pointer"
+                        color="gray.300"
+                        fontSize="20px"
+                        onClick={handleDeleteSearch}
+                      />
+                    }
+                    style={{
+                      position: 'absolute',
+                      top: '50%',
+                      transform: 'translateY(-50%)'
+                    }}
                   />
-                }
-                style={{
-                  position: 'absolute',
-                  top: '50%',
-                  transform: 'translateY(-50%)'
-                }}
-              />
-            )}
-            <InputLeftElement
-              pointerEvents="none"
-              children={<Icon as={FaSearch} color="gray.300" />}
-              style={{
-                position: 'absolute',
-                top: '50%',
-                transform: 'translateY(-50%)'
-              }}
-            />
-          </InputGroup>
+                )}
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<Icon as={FaSearch} color="gray.300" />}
+                  style={{
+                    position: 'absolute',
+                    top: '50%',
+                    transform: 'translateY(-50%)'
+                  }}
+                />
+              </InputGroup>
+              {/*------------------- Save Button -----------------*/}
 
-          <BoxWrapper w="100%">
-            <Flex w="100%" justify="space-between" align="center">
-              <Heading as="h3" fontSize="md">
-                Filtereinstellungen
-              </Heading>
-              <Tooltip label="Filter zurücksetzen" margin="bottom">
-                <Button
-                  h="0"
-                  onClick={handleFilterReset}
-                  colorScheme="transparent"
-                  paddingRight="0"
-                >
-                  <FaSync color="teal"></FaSync>
-                </Button>
-              </Tooltip>
+              <Button
+                alignSelf="end"
+                width={'fit-content'}
+                colorScheme="teal"
+                type="submit"
+                disabled={isSubmitting}
+                leftIcon={<FaSearch />}
+              >
+                Suchen
+              </Button>
             </Flex>
-            {isBoxOpen && (
+
+            <BoxWrapper w="100%">
+              <Flex w="100%" justify="space-between" align="center">
+                <Heading as="h3" fontSize="md">
+                  Filtereinstellungen
+                </Heading>
+                <Tooltip label="Filter zurücksetzen" margin="bottom">
+                  <Button
+                    h="0"
+                    onClick={handleFilterReset}
+                    colorScheme="transparent"
+                    paddingRight="0"
+                  >
+                    <FaSync color="teal"></FaSync>
+                  </Button>
+                </Tooltip>
+              </Flex>
+
               <Box w="100%">
-                <Flex w="100%" flexDir={'column'} gap="1rem">
+                <Flex
+                  flexDir={{ base: 'column', sm: 'row' }}
+                  justify={'space-between'}
+                  w="80%"
+                  gap="1rem"
+                >
                   {/* Filter properties checkboxes */}
                   <Flex flexDir="column">
-                    <Heading mb="0.5rem" as="h3" fontSize="md">
-                      Kategorie:
-                    </Heading>
-                    <Checkbox
-                      onChange={() => handlePropertyChange('popularity')}
-                      isChecked={selectedFilterProperty === 'popularity'}
-                    >
-                      Beliebtheit
-                    </Checkbox>
-                    <Checkbox
-                      onChange={() => handlePropertyChange('size')}
-                      isChecked={selectedFilterProperty === 'size'}
-                    >
-                      Anzahl der Fragen
-                    </Checkbox>
-                    <Checkbox
-                      onChange={() => handlePropertyChange('updatedAt')}
-                      isChecked={selectedFilterProperty === 'updatedAt'}
-                    >
-                      Letztes Update
-                    </Checkbox>
+                    <CustomCheckboxGroup
+                      labels={['Beliebtheit', 'Anzahl der Fragen', 'Letztes Update']}
+                      propertyValues={['popularity', 'size', 'updatedAt']}
+                      heading="Kategorie:"
+                      handleCheckboxChange={handlePropertyChange}
+                      isMandatory={false}
+                      selectedCheckbox={selectedFilterProperty}
+                    />
                   </Flex>
-
                   {/* Filter sortOrder checkboxes */}
                   <Flex flexDirection="column">
-                    <Heading mb="0.5rem" as="h3" fontSize="md">
-                      Sortieren:
-                    </Heading>
-                    <Checkbox
+                    <CustomCheckboxGroup
+                      labels={['Aufsteigend', 'Absteigend']}
+                      propertyValues={['asc', 'desc']}
+                      heading="Sortieren:"
                       disabled={!selectedFilterProperty}
-                      onChange={() => handleSortChange('asc')}
-                      isChecked={selectedSortOrder === 'asc'}
-                    >
-                      Aufsteigend
-                    </Checkbox>
-                    <Checkbox
-                      disabled={!selectedFilterProperty}
-                      onChange={() => handleSortChange('desc')}
-                      isChecked={selectedSortOrder === 'desc'}
-                    >
-                      Absteigend
-                    </Checkbox>
+                      handleCheckboxChange={handleSortChange}
+                      isMandatory={true}
+                      selectedCheckbox={selectedSortOrder}
+                    />
                   </Flex>
                 </Flex>
                 <Flex mt="1rem" w="100%" flexDir={'column'} gap="1rem">
-                  {/* Course of Study menu */}
-                  <Menu matchWidth={true} autoSelect={false}>
-                    <MenuButton w="100%" as={Button} rightIcon={<FaChevronDown />}>
-                      <Flex display="flex" alignItems="center" justify="center">
+                  {/*------------------- Course Of Study --------------------*/}
+                  <InputGroup>
+                    <Tooltip label="Studiengang">
+                      <InputLeftAddon>
                         <FaGraduationCap />
-                        <Text
-                          fontWeight="bold"
-                          color="white"
-                          _dark={{
-                            color: 'gray.500',
-                            borderColor: 'gray.500',
-                            _hover: { color: 'gray.400', borderColor: 'gray.400' }
-                          }}
-                          ml="0.5rem"
-                        >
-                          Studiengang
-                        </Text>
-                      </Flex>
-                    </MenuButton>
-                    <MenuList>
-                      {courseOfStudy.map((course) => (
-                        <MenuItem key={course.name}>
-                          <Checkbox
-                            isChecked={selectedCourse === course.name}
-                            onChange={() => handleCourseChange(course.name)}
-                          >
-                            {course.name}
-                          </Checkbox>
-                        </MenuItem>
-                      ))}
-                    </MenuList>
-                  </Menu>
-                  {/* Module menu */}
-                  <Menu matchWidth={true} closeOnSelect={false} autoSelect={false}>
-                    <MenuButton
-                      w="100%"
-                      isDisabled={!selectedCourse}
-                      as={Button}
-                      rightIcon={<FaChevronDown />}
+                      </InputLeftAddon>
+                    </Tooltip>
+                    <Select
+                      value={selectedCourseOfStudy}
+                      defaultValue={''}
+                      onChange={handleCourseOfStudyChange}
                     >
-                      <Flex display="flex" alignItems="center" justify="center">
+                      <option value="" disabled hidden>
+                        Studiengang auswählen
+                      </option>
+                      {courseOfStudy.map(({ name }) => (
+                        <option value={name} key={name}>
+                          {name}
+                        </option>
+                      ))}
+                    </Select>
+                  </InputGroup>
+
+                  {/*------------------- Course --------------------*/}
+                  <InputGroup>
+                    <Tooltip label="Modul">
+                      <InputLeftAddon>
                         <FaBook />
-                        <Text
-                          fontWeight="bold"
-                          color="white"
-                          _dark={{
-                            color: 'gray.500',
-                            borderColor: 'gray.500',
-                            _hover: { color: 'gray.400', borderColor: 'gray.400' }
-                          }}
-                          ml="0.5rem"
-                        >
-                          Module
-                        </Text>
-                      </Flex>
-                    </MenuButton>
-                    <MenuList maxH="19rem" overflowY="auto">
+                      </InputLeftAddon>
+                    </Tooltip>
+                    <Select
+                      value={selectedCourse}
+                      defaultValue={''}
+                      isDisabled={!selectedCourseOfStudy}
+                      onChange={handleCourseChange}
+                    >
+                      <option value="" disabled hidden>
+                        Module auswählen
+                      </option>
                       {courseOfStudy
-                        ?.find(({ name }) => name === selectedCourse)
+                        ?.find(({ name }) => name === selectedCourseOfStudy)
                         ?.courses?.map(({ name }) => (
-                          <MenuItem key={name}>
-                            <Checkbox
-                              isChecked={selectedModules.includes(name)}
-                              onChange={() => handleModuleChange(name)}
-                            >
-                              {name}
-                            </Checkbox>
-                          </MenuItem>
+                          <option key={name} value={name}>
+                            {name}
+                          </option>
                         )) || []}
-                    </MenuList>
-                  </Menu>
+                    </Select>
+                  </InputGroup>
                 </Flex>
               </Box>
-            )}
-          </BoxWrapper>
-          <Flex w="100%" justify="center" h="20%">
-            {/* ________________ Collapse Button ____________________ */}
-            <Tooltip
-              label={!isBoxOpen ? 'Filter öffnen' : 'Filter schließen'}
-              placement="bottom"
-            >
-              <IconButton
-                display={{ base: 'flex', md: 'flex' }}
-                height="20%"
-                top={isBoxOpen ? '-1.5rem' : '-1.5rem'}
-                borderRadius="xl"
-                onClick={handleToggleBox}
-                aria-label="Toggle Sidebar"
-                fontSize="1.1rem"
-                size="sm"
-                boxShadow={isBoxOpen ? 'none' : '1px 0 0 0.2px rgba(0, 0, 0,0.1)'}
-                colorScheme="gray"
-                bg="white"
-                color="teal"
-                _hover={{ bg: 'white' }}
-                _dark={{
-                  bg: 'gray.800',
-                  color: 'teal.200',
-                  boxShadow: 'none',
-                  borderRight: boxShadowDark,
-                  _hover: { bg: 'gray.800' }
-                }}
-              >
-                {isBoxOpen ? <FaArrowUp /> : <FaArrowDown />}
-              </IconButton>
-            </Tooltip>
+            </BoxWrapper>
           </Flex>
-        </Flex>
+        </form>
       </Box>
 
       {isLoading ? (
